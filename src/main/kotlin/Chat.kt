1679145dd81e570aac9ce3d2f7c1bebc57f5d018
Vladbor787 <borovik_nm@mail.ru>
2022-05-20 00:05:50 +0300

    project structure created

---
+++ src/main/kotlin/Chat.kt
@@ -0,0 +1,5 @@
+data class Chat(
+    val chatId: Int,
+    val messages: MutableList<Message>,
+    val userId: Int
+)
---
+++ src/main/kotlin/Message.kt
@@ -0,0 +1,6 @@
+data class Message(
+    val messageId: Int,
+    var text: String,
+    var isRead: Boolean,
+    val userID: Int
+)
